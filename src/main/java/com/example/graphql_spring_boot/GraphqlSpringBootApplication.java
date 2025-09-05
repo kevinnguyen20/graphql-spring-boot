5574c6fba1a4096b633358eb03008030a71de524
Kevin Nguyen <dev.kk.nguyen@gmail.com>
2025-09-05 13:06:13 +0200

    Query mappings

--- src/main/java/com/example/graphql_spring_boot/GraphqlSpringBootApplication.java
+++ src/main/java/com/example/graphql_spring_boot/GraphqlSpringBootApplication.java
@@ -2,9 +2,10 @@ package com.example.graphql_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.graphql.execution.RuntimeWiringConfigurer;
-import org.springframework.stereotype.Service;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
 
 import java.util.Collection;
 import java.util.List;
@@ -12,35 +13,44 @@ import java.util.List;
 @SpringBootApplication
 public class GraphqlSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GraphqlSpringBootApplication.class, args);
-	}
-
-    @Bean
-    RuntimeWiringConfigurer runtimeWiringConfigurer(CrmService crm) {
-        return builder -> {
-            builder.type("Customer", wiring -> wiring.dataFetcher("profile", env -> crm.getProfile(env.getSource())));
-            builder.type("Query", wiring -> wiring.dataFetcher("customerById", env -> crm.getCustomerById(Integer.parseInt(env.getArgument("id")))));
-            builder.type("Query", wiring -> wiring.dataFetcher("customers", env -> crm.getCustomers()));
-        };
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlSpringBootApplication.class, args);
     }
 }
 
-record Customer(Integer id, String name) {}
+@Controller
+class GreetingsController {
 
-record Profile(Integer id, Integer customerId) {}
+    private final List<Customer> customerList = List.of(
+            new Customer(1, "A"),
+            new Customer(2, "B")
+    );
 
-@Service
-class CrmService {
-    Profile getProfile(Customer customer) {
-        return new Profile(customer.id(), customer.id());
-    }
-    Customer getCustomerById(Integer id) {
+    @QueryMapping
+    Customer customerById(@Argument Integer id) {
         return new Customer(id, Math.random() > .5 ? "A" : "B");
     }
 
-    Collection<Customer> getCustomers() {
-        return List.of(new Customer(1, "A"),
-                new Customer(2, "B"));
+    @QueryMapping
+    String helloWithName(@Argument String name) {
+        return "Hello " + name + "!";
+    }
+
+    @QueryMapping
+    String hello() {
+        return "Hello World!";
+    }
+
+    @QueryMapping
+    Collection<Customer> customers() {
+        return this.customerList;
+    }
+
+    @SchemaMapping(typeName = "Customer")
+    Account account(Customer customer) {
+        return new Account(customer.id());
     }
 }
+
+record Account(Integer id){}
+record Customer(Integer id, String name) {}
